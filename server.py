3857c707b2dcfb80b7cc0b1c27af6fee29dd3726
orihalalmit <39694627+orihalalmit@users.noreply.github.com>
2019-11-29 11:35:19 +0200

    Documentation

--- server.py
+++ server.py
@@ -1,27 +1,31 @@
 from flask import Flask, request, jsonify, session
-from datetime import date
+from datetime import datetime
 from dataclasses import dataclass
 from functools import wraps
 
-last_message_id = 0
 
+
+# Class for storing A single Message
 @dataclass
 class Message:
     sender: str
     receiver: str
     message: str
     subject: str
-    creation_date: date 
+    creation_date: datetime 
     is_read: bool
     id: int
 
+# App initialization 
 app = Flask(__name__)
 app.secret_key = 'super secret key'
 app.config['SESSION_TYPE'] = 'filesystem'
 
+# Global variable to store the messages from the clients
 messages = []
+last_message_id = 0
 
-
+# Authentication check 
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
@@ -31,9 +35,10 @@ def login_required(f):
             return "Login required"
     return wrap
 
+
+# Login
 @app.route('/login', methods=['POST'])
 def login():
-    #req_data = request.get_json()
     if session.get('username') == request.args.get('username'):
         return "You are already logged in"
     session['username'] = request.args.get('username')
@@ -41,7 +46,7 @@ def login():
     return "Login successfully"
 
 
-
+# Logout
 @app.route('/logout')
 @login_required
 def logout():
@@ -49,28 +54,32 @@ def logout():
         del session['username']
     return "Logout successfully"
 
+# Write a message
+# adds to messages the new messages 
+# each message gets unique id
 @app.route('/write_message')
 @login_required
 def write_message():
     global messages
     global last_message_id
-    #req_data = request.get_json()
     messages.append(Message(session['username'],
                             request.args.get('receiver'),
                             request.args.get('message'),
                             request.args.get('subject'),
-                             date.today(),
+                             datetime.now(),
                              False,
                              last_message_id + 1))
     last_message_id += 1
     return "Message received"
 
+# Return to the user all the read and unread messages with json format
 @app.route('/get_all_messages')
 @login_required
 def get_all_messages():
     global messages
     return jsonify(list(filter(lambda message: message.receiver == session['username'], messages)))
 
+# Return only unread messages
 @app.route('/get_all_unread_messages')
 @login_required
 def get_all_unread_messages():
@@ -82,6 +91,7 @@ def get_all_unread_messages():
         message.is_read = True
     return jsonify(filters_messages)
 
+# Return next unread message (if exists)
 @app.route('/read_message')
 @login_required
 def read_message():
@@ -95,10 +105,11 @@ def read_message():
     else:
         return "No unread messages"
 
+# Delete a message by message id
+# only receiver or sender can delete
 @app.route('/delete_message')
 @login_required
 def delete_message():
-    #req_data = request.get_json()
     message_to_delete = list(filter(lambda message:
                                     (message.receiver == session['username']
                                      or message.sender == session['username'])
@@ -111,5 +122,6 @@ def delete_message():
     else:
         return "Message not found"
 
+# runs the app
 if __name__ == '__main__':
-    app.run(port=80, debug=True)
+    app.run(port=80)
